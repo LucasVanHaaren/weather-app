da0b2046d4c01099bac9cfe0a3b96cb46547f514
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2021-02-11 11:38:51 +0100

    application boilerplate
    * add vuetify dependency
    * fix fontawesome integrity check
    * add application layout
    * add placeholder views
    * add application routes
    * add default TodayWeather route

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark fixed flat dense>
+      <v-btn text plain :to="{ name: 'root' }">
+        <v-icon>fas fa-cloud</v-icon>
+        <span class="ml-1">WeatherApp</span>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-text-field
+          solo-inverted
+          flat
+          dense
+          hide-details
+          clearable
+          label="Search"
+          prepend-inner-icon="fas fa-search"
+          v-model="search"
+          @keydown.enter="routeToCityName"
+        ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn text plain :to="{ name: 'Settings' }">
+        <span class="mr-2 hidden-md-and-down">Settings</span>
+        <v-icon>fas fa-cog</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view>
+        </router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import router from "@/router"
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  data: () => ({
+    search: ""
+  }),
+  methods: {
+    routeToCityName() {
+      router.push({name: router.name, params: { city: this.search.toLowerCase() } })
+      this.search = ""
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,42 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'root',
+    redirect: { name: 'TodayWeatherDefault' }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/today-weather',
+    name: 'TodayWeatherDefault',
+    redirect: { name: 'TodayWeather', params: { city: 'paris' } }
+  },
+  {
+    path: '/today-weather/:city',
+    name: 'TodayWeather',
+    component: () => import(/* webpackChunkName: "todayweather" */ '../views/TodayWeatherView.vue')
+  },
+  {
+    path: '/week-weather/:city',
+    name: 'WeekWeather',
+    component: () => import(/* webpackChunkName: "weekweather" */ '../views/WeekWeatherView.vue')
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import(/* webpackChunkName: "settings" */ '../views/SettingsView.vue')
+  },
+  {
+    path: '*',
+    redirect: { name : 'root' }
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
